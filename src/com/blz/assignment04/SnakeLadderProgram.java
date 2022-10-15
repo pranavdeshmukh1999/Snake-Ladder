376360e6e6cb93d08304ee429d443d317fa25fa6
pranavdeshmukh1999 <pranavdeshmukh182@gmaill.com>
2022-10-15 12:51:33 +0530

    Added : WELCOME MESSAGE

---
+++ src/com/blz/assignment04/SnakeLadderProgram.java
@@ -0,0 +1,12 @@
+package com.blz.assignment04;
+
+
+public class SnakeLadderProgram {
+
+	public static void main(String args[]) {
+
+		System.out.println("Welcome To Snake And Ladder Simulater program");
+		
+	}
+}
+
