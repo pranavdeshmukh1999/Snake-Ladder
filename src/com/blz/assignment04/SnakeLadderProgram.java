497497a7ee5ea44d80ae016a5a076cfa94a85af8
pranavdeshmukh1999 <pranavdeshmukh182@gmaill.com>
2022-10-16 17:30:02 +0530

    Added:Check Option

--- src/com/blz/assignment04/SnakeLadderProgram.java
+++ src/com/blz/assignment04/SnakeLadderProgram.java
@@ -4,27 +4,59 @@ import java.util.Random;
 
 public class SnakeLadderProgram {
 
+	static final int ladder = 1;
+	static final int snake = 2;
+	static int noplay = 0;
+
 	public static void main(String args[]) {
 //WELCOME MESSAGE FOR SNAKE & LADDDER PROGRAM
 		System.out.println("Welcome To Snake And Ladder Simulater program");
-		
-		int position=0;
-		int count=0;
-		
-		System.out.println("Start position="+position);
-		
-		Random r=new Random();
-		
-		int dice=r.nextInt(6)+1;
-		
-		System.out.println("Dice outcome is="+dice);
-		
+
+//		UC--1
+		int position = 0;
+		int count = 0;
+
+		System.out.println("Start position=" + position);
+
+		Random r = new Random();
+
+		int dice = r.nextInt(6) + 1;
+
+		System.out.println("Dice outcome is=" + dice);
+
 		count++;
-		
-		System.err.println("Total count="+count);
-	}
 
+		System.err.println("Total count=" + count);
 
-		
+// PLAYER CHECK FOR OPTION uc-3
+
+		System.out.println("Start position=" + position);
+
+		System.out.println("Dice outcome is=" + dice);
+
+		int optionCheck = r.nextInt(3);
+
+		System.out.println("Option Check=" + optionCheck);
+
+		if (optionCheck == ladder) {
+
+			System.err.println("It is Ladder");
+
+			position = position + dice;
+
+		} else if (optionCheck == snake) {
+
+			System.err.println("It Is snake");
+
+			position = position - dice;
+		} else {
+
+			System.out.println("No play");
+		}
+
+		count++;
+
+		System.err.println("Toatl Count= " + count);
 	}
+}
 
